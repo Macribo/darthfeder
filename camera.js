87051553bb2495bbdff1ee9e89f3e6006d829cb6
macribo <roibeard.mac.unfraidh@gmail.com>
2017-03-24 13:36:14 +0100

    Split camera from tileworld (separation of concerns). Modify tileset.

--- camera.js
+++ camera.js
@@ -1,3 +1,100 @@
 //jshint esversion:6
-module.exports = function camera(tileSize, county, grid, vw, vh, mapSymbolToTerrainType){
+
+function createArrayOfRandomInts(length,upperbound){
+    let randomNumbers = Array(length);
+    for (let i = 0; i<randomNumbers.length; i++){
+        randomNumbers[i] = Math.floor(Math.random()*upperbound);
+    }
+    return randomNumbers;
+}
+
+module.exports = function Camera(tileSize, county, grid, vw, vh, mapSymbolToTerrainType){
+
+    // minimum number of tiles to cover the
+    // viewport under all circumstances
+    let tw = Math.floor(vw / tileSize + 1);
+    let th = Math.floor(vh / tileSize + 1);
+    createGrid(tw, th);
+    let randomTileVariations = createArrayOfRandomInts(100,4);
+
+    function setOffsets(ox, oy){
+        grid.style.left =`${-ox % tileSize}px`;
+        grid.style.top = `${-oy % tileSize}px`;
+        let sx = Math.floor(ox / tileSize);
+        let sy = Math.floor(oy / tileSize);
+
+        updateGrid(tw, th, county, sx, sy);
+    }
+
+    function createGrid(tw, th){
+        //console.log(`Creating tile grid ${tw}x${th}`);
+        for(let tileY = 0; tileY < th; tileY++){ 
+            for(let tileX = 0; tileX < tw; tileX++){
+                let tile = createTile(tileX, tileY, 0,0);
+
+                grid.appendChild(tile);
+            }
+        }
+    }
+
+    function createTile(tileX, tileY, terrainType, terrainVariation ){
+        // console.log(`CreateTile ${tileX}, ${tileY}`); 
+        let tile = document.createElement("div");
+        tile.setAttribute("id", `tile${tileX}_${tileY}`); //template strings
+        tile.classList.add('tile');  
+        let backgroundPosX = -terrainVariation * tileSize;
+        let backgroundPosY = -terrainType * tileSize;
+        tile.style.backgroundPositionX = `${backgroundPosX}px`;
+        tile.style.backgroundPositionY = `${backgroundPosY}px`;
+        tile.style.left = tileX*tileSize+ "px";
+        tile.style.top = tileY*tileSize + "px";
+
+        return tile;
+    }
+
+
+    function updateGrid(tw, th, map, sx, sy){
+        for(let tileY = 0; tileY < th; tileY++){ 
+            for(let tileX = 0; tileX < tw; tileX++){
+                let mapY =tileY + sy;
+                let mapX =tileX + sx;
+
+                let variationIndex = mapX * mapY;
+                let terrainVariation = randomTileVariations[variationIndex % randomTileVariations.length];
+                
+                let mapSymbol;
+                if (mapY>=map.length){
+                    mapSymbol=undefined;
+                } else {
+                    mapSymbol = map[mapY][mapX];   //tileY gives us the map line, tileX gives the character position    
+                }
+                
+                let terrainType = mapSymbolToTerrainType(mapSymbol);
+
+                let tile = document.querySelector(`#tile${tileX}_${tileY}`);
+
+                if (terrainType !== undefined) {
+                    // show tile
+                    // let terrainVariation =0 ;
+                    if(mapSymbol === '>'){
+                        terrainVariation = mapY;
+                        terrainVariation = (terrainVariation + Math.floor(Date.now() / 500)) % 4;
+                    }
+                    let backgroundPosX = -terrainVariation * tileSize;
+                    let backgroundPosY = -terrainType * tileSize;
+                    tile.style.backgroundPositionX = `${backgroundPosX}px`;
+                    tile.style.backgroundPositionY = `${backgroundPosY}px`;
+                    tile.style.opacity=1;
+                } else {
+                    // hide tile
+                    tile.style.opacity=0;
+                }
+
+            }
+        }
+    }
+
+    return {
+        setOffsets: setOffsets
+    };
 };
--- tileworld.js
+++ tileworld.js
@@ -1,6 +1,6 @@
 //jshint esversion:6
 
-const camera = require("./camera");
+const Camera = require("./camera");
 const maps = require('./maps');
 
 let county = maps.cork.split("\n");
@@ -22,9 +22,16 @@ const tileSize = 32;
 const vw = 32 * 32+ 10;
 const vh = 19 * 32 + 5;
 let grid = document.querySelector('#grid');
-camera(tileSize, county, grid, vw, vh, mapSymbolToTerrainType);
+let camera = Camera(tileSize, county, grid, vw, vh, mapSymbolToTerrainType);
 
+let ox=0, oy=0;
 
+function timerLoop() {
+    camera.setOffsets(ox, oy);
+    requestAnimationFrame(timerLoop);
+}
+
+requestAnimationFrame(timerLoop);
 /*
 function createWorldMap(map){
     let mapHeight = map.length;
@@ -44,153 +51,6 @@ function createWorldMap(map){
 }
 */
 
-
-//working with requestAnimationFrame
-var timers = [];
-
-timers.push({
-    //this timer fires every
-    delay: 50,
-
-    //fire this timer when requestAnimationFrame's timestamp reaches nextFireTime
-    nextFireTime:0,
-
-    //what does this timer do?
-    //it executes the doTimers function when this timer fires
-    doFunction: doTimers,
-});
-
-
-//the animation loop
-//the loop automatically receives the current time
-function timerLoop(currentTime){
-    //schedule another frame
-    //this is required to make the loop continue
-    //(without another requestAnimationFrame, the loop stops)
-    requestAnimationFrame(timerLoop);
-
-    //iterate through each timer object
-    for(let i = 0; i<timers.length; i++){
-
-        //if the currentTime is > this timer's nextFireTime
-
-        if (currentTime > timers[i].nextFireTime){
-            var t= timers[i];
-
-            //increment nextFireTime
-            t.nextFireTime=currentTime+t.delay;
-
-            //execute the task specified in this timer
-            t.doFunction(t,i);
-        }
-    }
-
-}
-
-requestAnimationFrame(timerLoop);
-
-function doTimers(){
-    grid.style.left =`${-ox % tileSize}px`;
-    grid.style.top = `${-oy % tileSize}px`;
-    let sx = Math.floor(ox / tileSize);
-    let sy = Math.floor(oy / tileSize);
-
-    updateGrid(tw, th, county, sx, sy);
-}
-
-function setOffset(offsetx,offsety){
-    body.style.backgroundPositionX = -offsetx + "px";
-    body.style.backgroundPositionY = -offsety + "px";
-}   
-
-function createArrayOfRandomInts(length,upperbound){
-    let randomNumbers = Array(length);
-    for (let i = 0; i<randomNumbers.length; i++){
-        randomNumbers[i] = Math.floor(Math.random()*upperbound);
-    }
-    return randomNumbers;
-}
-function createGrid(tw, th){
-    //console.log(`Creating tile grid ${tw}x${th}`);
-    for(let tileY = 0; tileY < th; tileY++){ 
-        for(let tileX = 0; tileX < tw; tileX++){
-            let tile = createTile(tileX, tileY, 0,0);
-
-            document.querySelector('#grid').appendChild(tile);
-        }
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-// minimum number of tiles to cover the
-// viewport under all circumstances
-let tw = Math.floor(vw / tileSize + 1);
-let th = Math.floor(vh / tileSize + 1);
-createGrid(tw, th);
-
-function createTile(tileX, tileY, terrainType, terrainVariation ){
-    // console.log(`CreateTiel ${tileX}, ${tileY}`); 
-    let tile = document.createElement("div");
-    tile.setAttribute("id", `tile${tileX}_${tileY}`); //template strings
-    tile.classList.add('tile');  
-    let backgroundPosX = -terrainVariation * tileSize;
-    let backgroundPosY = -terrainType * tileSize;
-    tile.style.backgroundPositionX = `${backgroundPosX}px`;
-    tile.style.backgroundPositionY = `${backgroundPosY}px`;
-    tile.style.left = tileX*tileSize+ "px";
-    tile.style.top = tileY*tileSize + "px";
-
-    return tile;
-}
-
-let randomTileVariations = createArrayOfRandomInts(100,4);
-
-
-function updateGrid(tw, th, map, sx, sy){
-    for(let tileY = 0; tileY < th; tileY++){ 
-        for(let tileX = 0; tileX < tw; tileX++){
-            let mapX =tileY + sy;
-            let mapY =tileX + sx;
-
-            let variationIndex = mapX * mapY;
-            let terrainVariation = randomTileVariations[variationIndex % randomTileVariations.length];
-            let mapSymbol = map[mapX][mapY];   //tileY gives us the map line, tileX gives the character position    
-            let terrainType = mapSymbolToTerrainType(mapSymbol);
-
-            let tile = document.querySelector(`#tile${tileX}_${tileY}`);
-
-            if (terrainType !== undefined) {
-                // show tile
-                // let terrainVariation =0 ;
-                if(mapSymbol === '>'){
-                    terrainVariation = mapY;
-                    terrainVariation = (terrainVariation + Math.floor(Date.now() / 500)) % 4;
-                }
-                let backgroundPosX = -terrainVariation * tileSize;
-                let backgroundPosY = -terrainType * tileSize;
-                tile.style.backgroundPositionX = `${backgroundPosX}px`;
-                tile.style.backgroundPositionY = `${backgroundPosY}px`;
-                tile.style.opacity=1;
-            } else {
-                // hide tile
-                tile.style.opacity=0;
-            }
-
-        }
-    }
-}
-
-
-let ox=0, oy=0;
 let px, py;
 window.addEventListener("keydown", function(event){
     // console.log('keycode', event.keyCode);
