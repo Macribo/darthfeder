c8c3ceb9d3f6aa01b256d6503593c232d54359ac
macribo <roibeard.mac.unfraidh@gmail.com>
2017-07-29 23:02:42 +0200

    Add playerBadge.js.

---
+++ contaePages/playerBadge.js
@@ -0,0 +1,7 @@
+console.log("hello playerBadge");
+
+
+var output = document.querySelector('#output');
+var player = document.querySelector('#player');
+var badgeSmall = document.querySelector('#badgeSmall');
+
