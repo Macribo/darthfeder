871330acaa41518ff5d0ecac64ab0f2060735389
macribo <roibeard.mac.unfraidh@gmail.com>
2017-07-30 10:33:29 +0200

    Implement little-shield to show player-team.

--- contaePages/playerBadge.js
+++ contaePages/playerBadge.js
@@ -1,7 +1,134 @@
-console.log("hello playerBadge");
-
-
+var pBadge = document.querySelector("#badgeSmall");
 var output = document.querySelector('#output');
-var player = document.querySelector('#player');
-var badgeSmall = document.querySelector('#badgeSmall');
+switch(output.innerHTML){ //replace with cookie data from character selection
 
+    case("Co. Loch Garman"): badgeSmall.style.backgroundPositionX ="-60px";
+        break;
+
+    case("Co. Uíbh Fhailí"): badgeSmall.style.backgroundPositionX ="-20px";
+        break;
+    case("Co. Lú"): badgeSmall.style.backgroundPositionX =-(20*2)+"px";
+        break;
+    case ("Co. Luimnigh"): badgeSmall.style.backgroundPositionX =-( 20*4)+ "px";
+        break;
+
+    case ("Co. Átha Cliath"): badgeSmall.style.backgroundPositionX =-( 20*5)+ "px";
+        break;
+    case ("Co. Liatroma"): badgeSmall.style.backgroundPositionX =-( 20*6)+ "px";
+        break;
+
+
+    case ("Co. Aontroma"): badgeSmall.style.backgroundPositionX =-( 20*7)+ "px";
+        break;
+
+
+    case ("Co. Dhún na nGall"): badgeSmall.style.backgroundPositionX =-( 20*8)+ "px";
+        break;
+
+
+    case ("Co. Fhear Manach"): badgeSmall.style.backgroundPositionX =-( 20*9)+ "px";
+        break;
+
+
+    case ("Co. Chorcaí"): badgeSmall.style.backgroundPositionX =-( 20*10)+ "px";
+        break;
+
+
+    case ("Co. Chill Mhantáin"): badgeSmall.style.backgroundPositionX =-( 20*11)+ "px";
+        break;
+
+
+    case ("Co. Ros Comáin"): badgeSmall.style.backgroundPositionX =-( 20*12)+ "px";
+        break;
+
+
+    case ("Co. Dhoire"): badgeSmall.style.backgroundPositionX =-( 20*13)+ "px";
+        break;
+
+
+    case ("Co. Shligigh"): badgeSmall.style.backgroundPositionX =-( 20*14)+ "px";
+        break;
+
+
+    case ("Co. Thiobraid Árann"): badgeSmall.style.backgroundPositionX =-( 20*15)+ "px";
+        break;
+
+
+    case ("Co. Laoise"): badgeSmall.style.backgroundPositionX =-( 20*16)+ "px";
+        break;
+
+
+    case ("Co. Phort Láirge"): badgeSmall.style.backgroundPositionX =-( 20*17)+ "px";
+        break;
+
+
+    case ("Co. Chill Chainnigh"): badgeSmall.style.backgroundPositionX =-( 20*18)+ "px";
+        break;
+
+
+    case ("Co. Na Mí"): badgeSmall.style.backgroundPositionX =-( 20*19)+ "px";
+        break;
+
+
+    case ("Co. Chiarraí"): badgeSmall.style.backgroundPositionX =-( 20*20)+ "px";
+        break;
+
+
+    case ("Co. An Longfoirt"): badgeSmall.style.backgroundPositionX =-( 20*21)+ "px";
+        break;
+
+
+    case ("Co. Mhaigh Eo"): badgeSmall.style.backgroundPositionX =-( 20*22)+ "px";
+        break;
+
+
+    case ("Co. Na hIarmhí"): badgeSmall.style.backgroundPositionX =-( 20*23)+ "px";
+        break;
+
+
+    case ("Co. An Dúin"): badgeSmall.style.backgroundPositionX =-( 20*24)+ "px";
+        break;
+
+
+    case ("Co. Ard Mhacha"): badgeSmall.style.backgroundPositionX =-( 20*25)+ "px";
+        break;
+
+    case ("Co. Cheatharlach"): badgeSmall.style.backgroundPositionX =-( 20*26)+ "px";
+        break;
+
+
+    case ("Co. Mhuineacháin"): badgeSmall.style.backgroundPositionX =-( 20*27)+ "px";
+        break;
+
+
+    case ("Co. na Gaillimhe"): badgeSmall.style.backgroundPositionX =-( 20*28)+ "px";
+        break;
+
+
+    case ("Co. Chill Dara"): badgeSmall.style.backgroundPositionX =-( 20*29)+ "px";
+        break;
+
+
+    case ("Co. An Chláir"): badgeSmall.style.backgroundPositionX =-( 20*30)+ "px";
+        break;
+
+
+    case ("Co. An Cabháin"): badgeSmall.style.backgroundPositionX =-( 20*31)+ "px";
+        break;
+
+
+    case ("Co. Thír Eoghain"): badgeSmall.style.backgroundPositionX =-( 20*32)+ "px";
+        break;
+
+
+    case ("Tír Thar Muir"): badgeSmall.style.backgroundPositionX =-( 20*33)+ "px";
+        break;
+
+
+    case ("An Teanga"): badgeSmall.style.backgroundPositionX =-( 20*34)+ "px";
+        break;
+
+
+
+
+}
