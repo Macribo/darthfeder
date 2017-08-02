1d928ef54472ec632719ddfb6f70d9fcde943b9e
macribo <roibeard.mac.unfraidh@gmail.com>
2017-08-02 19:23:52 +0200

    Implement location highlighting in Co.Cork

--- contaePages/places.js
+++ contaePages/places.js
@@ -1,15 +1,2 @@
 //jshint esversion:6
-module.exports = function places(){
-switch(output.innerHTML){
 
-    case("Co. Chorcaí"): console.log("Hello Corcaigh");
-
-
-
-        break;
-    default: console.log("hello from places.js");
-
-
-}
-
-};
